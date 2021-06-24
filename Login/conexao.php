7f529280c61c105df76d4250af1e1d1a0c6b8d1f
gustavojms <gustavo.joset0@hotmail.com>
2021-06-24 14:03:50 -0300

    arquivo de conexao

--- Login/conexao.php
+++ Login/conexao.php
@@ -1,7 +1,7 @@
 <?php
 define('HOST', 'localhost');
 define('USUARIO', 'root');
-define('SENHA', '20121b6-rc0246');
+define('SENHA', 'senha');
 define('DB', 'login');
 
 $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ("Erro de conexão");
