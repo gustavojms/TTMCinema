2381e098b2a9f7d9f52779c22a777c95770821aa
gustavojms <78878268+gustavojms@users.noreply.github.com>
2021-06-28 22:32:08 -0300

    delete

--- SQL/Cadastro/conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('HOST', 'localhost');
-define('USUARIO', 'root');
-define('SENHA', '20121b6-rc0246');
-define('DB', 'login');
-
-$conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ("Erro de conexão");
-?>
