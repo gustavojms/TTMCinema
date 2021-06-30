70743153a705419e04b1c9cb145ed5784f9f8d90
gustavojms <gustavo.joset0@hotmail.com>
2021-06-30 00:05:17 -0300

    update index

--- crud/cadastro/index.php
+++ crud/cadastro/index.php
@@ -5,15 +5,36 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastrar</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
+<main>
+    <h1>Cadastrar</h1>
+
     <form action="add.php" method="POST">
-        <input type="text" name="nome" placeholder="nome">
-        <input type="text" name="usuario" placeholder="usuario">
-        <input type="password" name="senha" placeholder="senha">
-        <input type="submit">
-    </form>    
+        <label for="nome">
+        <span>Nome</span>
+        <input type="text" name="nome" id="nome">
+        </label>
+
+        <label for="usuario">
+        <span>Usuario</span>
+        <input type="text" name="usuario" id="usuario">
+        </label>
+
+        <label for="senha">
+        <span>Senha</span>
+        <input type="password" name="senha" id="senha">
+        </label>
+
+        <input type="submit" value="Cadastrar">
+
+
+    </form>
+
+</main> 
+<section class="images"></section>
 </body>
 
 </html>
