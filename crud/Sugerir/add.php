78fa80a2228a0d0d43d96af41d5ede0a8887bef6
gustavojms <gustavo.joset0@hotmail.com>
2021-06-30 00:09:46 -0300

    update files

--- crud/Sugerir/add.php
+++ crud/Sugerir/add.php
@@ -1,7 +1,7 @@
 <?php 
     $nome = $_POST['nome_filme'];
     $data_filme = $_POST['lancamento'];
-    $data = $nome . "," . $data_filme . "\n";
+    $data = $nome . "," . $data_filme . "," . "\n";
 
     $fp = fopen('source.csv', 'a');
     fwrite($fp, $data);
--- crud/Sugerir/index.php
+++ crud/Sugerir/index.php
@@ -5,14 +5,29 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sugerir</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
+
+    <main>
+        <h1>Qual filme gostaria de sugerir?</h1>
     <form action="add.php" method="POST">
-        <input type="text" name="nome_filme" placeholder="Digite o nome do filme">
-        <input type="text" name="lancamento" placeholder="Digite a data de lancamento">
-        <button type="submit">Sugerir</button>
-    </form>    
+        <label for="nome_filme">
+        <span>Digite o nome do filme</span>
+        <input type="text" name="nome_filme" id="nome_filme">
+        </label>
+
+        <label for="lancamento">
+        <span>Informe a data do lancamento</span>
+        <input type="date" name="lancamento" id="lancamento">
+        </label>
+
+        <input type="submit" value="Sugerir">
+        <a href="sugestoes.php">Ver sugestoes</a>
+    </form>
+
+    </main>    
 </body>
 
 </html>
