8da0fd7961b9f5764f1efa1111f0ff6bce2c5784
gustavojms <gustavo.joset0@hotmail.com>
2021-07-13 16:03:43 -0300

    user files

---
+++ usuario/logar.php
@@ -0,0 +1,25 @@
+<?php 
+
+    if (isset($_POST['usuario']) && !empty($_POST['usuario']) && isset($_POST['senha']) && !empty($_POST['senha'])) {
+        require 'conexao/conexao.php';
+        require 'UsuarioClass.php';
+
+        $user = new Usuario();
+
+        $usuario = addslashes($_POST['usuario']);
+        $senha = addslashes($_POST['senha']);
+
+        if($user -> login($usuario, $senha) === true) {
+            if(isset($_SESSION['userL'])) {
+                header("Location: painel.php");
+            }
+            header("Location: painel.php");
+        } else {
+            header("Location: login.php");
+        }
+
+} else {
+    header("Location: login.php");
+}
+
+?>
