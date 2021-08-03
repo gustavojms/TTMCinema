581da359d7d917fb44e78aa945980f2f2ca0aa4a
gustavojms <gustavo.joset0@hotmail.com>
2021-08-03 16:53:51 -0300

    Modificaçao na navbar

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php 
     include 'conexao/conexao.php';
+    include 'usuario/UsuarioClass.php';
 ?>
 
 <!DOCTYPE html>
@@ -22,12 +23,21 @@
                 <div class="line2"></div>
                 <div class="line3"></div>
             </div>
+            <?php if (!Usuario::isLogged()): ?>
             <ul class="nav-list">
                 <li><a href="index.php">Inicio</a></li>
                 <li><a href="usuario/cadastro.php">Cadastrar</a></li>
                 <li><a href="usuario/login.php">Entrar</a></li>
                 <li><a href="catalogo/catalogo.php">Catalogo</a></li>
             </ul>
+            <?php else: ?>
+            <ul class="nav-list">
+                <li><a href="index.php">Inicio</a></li>
+                <li><a href="catalogo/catalogo.php">Catalogo</a></li>
+                <li><a href="usuario/login.php">Entrar</a></li>
+                <li><a href="usuario/logout.php">Sair</a></li>
+            </ul>
+            <?php endif ?>
         </nav>
     </header>
     <section class="images">
