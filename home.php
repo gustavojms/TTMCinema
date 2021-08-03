c39ddef93f1725f079986675114cccc85fb4814e
gustavojms <gustavo.joset0@hotmail.com>
2021-08-03 16:35:35 -0300

    Apenas usuario logado e modificaçao no navbar

--- home.php
+++ home.php
@@ -1,5 +1,10 @@
 <?php 
     include 'conexao/conexao.php';
+    include '../usuario/UsuarioClass.php';
+    if(!Usuario::isLogged()) {
+        header("Location: /usuario/login.php");
+        exit();
+    }
 ?>
 
 <!DOCTYPE html>
@@ -23,9 +28,9 @@
             </div>
             <ul class="nav-list">
                 <li><a href="/index.php">Inicio</a></li>
-                <li><a href="/usuario/cadastro.php">Cadastrar</a></li>
-                <li><a href="/usuario/login.php">Entrar</a></li>
                 <li><a href="/catalogo/catalogo.php">Catalogo</a></li>
+                <li><a href="/usuario/login.php">Entrar</a></li>
+                <li><a href="/usuario/logout.php">Sair</a></li>
             </ul>
         </nav>
 </header>
