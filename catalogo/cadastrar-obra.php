2f6a24f7593eed0291d024c663438073dea9e534
gustavojms <gustavo.joset0@hotmail.com>
2021-08-03 17:04:59 -0300

    Modificação na navbar

--- catalogo/cadastrar-obra.php
+++ catalogo/cadastrar-obra.php
@@ -28,12 +28,21 @@
                 <div class="line2"></div>
                 <div class="line3"></div>
             </div>
+            <?php if (!Usuario::isLogged()): ?>
             <ul class="nav-list">
                 <li><a href="/index.php">Inicio</a></li>
                 <li><a href="/usuario/cadastro.php">Cadastrar</a></li>
                 <li><a href="/usuario/login.php">Entrar</a></li>
                 <li><a href="/catalogo/catalogo.php">Catalogo</a></li>
             </ul>
+            <?php else: ?>
+            <ul class="nav-list">
+                <li><a href="/index.php">Inicio</a></li>
+                <li><a href="/catalogo/catalogo.php">Catalogo</a></li>
+                <li><a href="/usuario/login.php">Entrar</a></li>
+                <li><a href="/usuario/logout.php">Sair</a></li>
+            </ul>
+            <?php endif ?>
         </nav>
 </header>
 <main>
