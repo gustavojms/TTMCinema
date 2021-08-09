06b311cf5dc338eed59c7466a3114aaf114ffa31
gustavojms <gustavo.joset0@hotmail.com>
2021-08-09 18:52:22 -0300

    Adicionado novos dados para armazenar

--- catalogo/add-obra.php
+++ catalogo/add-obra.php
@@ -6,9 +6,12 @@
     $ano_lancamento = addslashes($_POST['ano_lancamento']);
     $etaria = addslashes($_POST['etaria']);
     $tipo = addslashes($_POST['tipo']);
+    $nacionalidade = addslashes($_POST['nacionalidade']);
+    $audio = addslashes($_POST['audio']);
+    $legenda = addslashes($_POST['legenda']);
 
-    $result = $pdo -> prepare("INSERT INTO obra (titulo, ano_lancamento, classificacao_etaria, tipo, id_usuario) VALUES (?, ?, ?, ?, ?)");
-    $result -> execute([$titulo, $ano_lancamento, $etaria, $tipo, $_SESSION['userId']]);
+    $result = $pdo -> prepare("INSERT INTO obra (titulo, ano_lancamento, classificacao_etaria, tipo, nacionalidade, audio, legenda, id_usuario_obra) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+    $result -> execute([$titulo, $ano_lancamento, $etaria, $tipo, $nacionalidade,$audio, $legenda, $_SESSION['userId']]);
 
     header("Location: cadastrar-obra.php");
 ?>
