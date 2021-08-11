6d7f768f8e00fb580005006f36386ab4df9a7270
gustavojms <gustavo.joset0@hotmail.com>
2021-08-11 19:15:52 -0300

    Adicionado método de segurança para invalidar cadastro em branco

--- catalogo/add-obra.php
+++ catalogo/add-obra.php
@@ -2,16 +2,26 @@
     include '../conexao/conexao.php';
     include '../usuario/UsuarioClass.php';
 
-    $titulo = addslashes($_POST['titulo']);
-    $ano_lancamento = addslashes($_POST['ano_lancamento']);
-    $etaria = addslashes($_POST['etaria']);
-    $tipo = addslashes($_POST['tipo']);
-    $nacionalidade = addslashes($_POST['nacionalidade']);
-    $audio = addslashes($_POST['audio']);
-    $legenda = addslashes($_POST['legenda']);
+    if(isset($_POST['titulo']) && !empty($_POST['titulo']) && isset($_POST['ano_lancamento']) && !empty($_POST['ano_lancamento']) 
+    && isset($_POST['etaria']) && !empty($_POST['etaria']) && isset($_POST['tipo']) && !empty($_POST['tipo']) 
+    && isset($_POST['nacionalidade']) && !empty($_POST['nacionalidade']) && isset($_POST['audio'])
+    && !empty($_POST['audio']) && isset($_POST['legenda']) && !empty($_POST['legenda'])) {
+
+        
+        $titulo = addslashes($_POST['titulo']);
+        $ano_lancamento = addslashes($_POST['ano_lancamento']);
+        $etaria = addslashes($_POST['etaria']);
+        $tipo = addslashes($_POST['tipo']);
+        $nacionalidade = addslashes($_POST['nacionalidade']);
+        $audio = addslashes($_POST['audio']);
+        $legenda = addslashes($_POST['legenda']);
+
+    } else {
+        header("Location: /catalogo/cadastrar-obra.php");
+    }
 
     $result = $pdo -> prepare("INSERT INTO obra (titulo, ano_lancamento, classificacao_etaria, tipo, nacionalidade, audio, legenda, id_usuario_obra) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
     $result -> execute([$titulo, $ano_lancamento, $etaria, $tipo, $nacionalidade,$audio, $legenda, $_SESSION['userId']]);
 
-    header("Location: cadastrar-obra.php");
+    header("Location: /catalogo/catalogo.php");
 ?>
