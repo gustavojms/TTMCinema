a897a6d94607ad1fc5345b459403572bc40e41ea
gustavojms <gustavo.joset0@hotmail.com>
2021-08-11 19:15:19 -0300

    Adicionado método de segurança para invalidar cadastro em branco

--- comentarios/add-comment.php
+++ comentarios/add-comment.php
@@ -1,10 +1,16 @@
 <?php
     include '../conexao/conexao.php';
+    $id = $_GET['id_obra'];
 
-    $comentario = addslashes($_POST['comentario']);
+    if (isset($_POST['comentario']) && !empty($_POST['comentario'])) {
+        $comentario = addslashes($_POST['comentario']);
+
+    } else {
+        header("Location: /catalogo/obras.php?id_obra=$id");
+    }
+
     $sql = $pdo -> query("SELECT id_obra FROM obra");
     $data = $sql -> fetchAll();
-    $id = $_GET['id_obra'];
 
     $result = $pdo -> prepare("INSERT INTO comentario (comentario, id_obra_cmt, id_user_cmt) VALUES(?, ?, ?)");
     $result -> execute([$comentario, $id, $_SESSION['userId']]);
