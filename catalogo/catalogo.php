b5b864799556ca04daa05b01ed837614644570c9
gustavojms <gustavo.joset0@hotmail.com>
2021-08-13 18:01:50 -0300

    Alteração na navbar

--- catalogo/catalogo.php
+++ catalogo/catalogo.php
@@ -11,7 +11,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Catalogo</title>
+    <title>Catalogo - TTMCinema</title>
     <link rel="stylesheet" href="/assets/css/style.css">
     <link rel="icon" href="/assets/img/moon.png" type="image/x-icon">
 </head>
@@ -24,21 +24,7 @@
                 <div class="line2"></div>
                 <div class="line3"></div>
             </div>
-            <?php if (!Usuario::isLogged()): ?>
-            <ul class="nav-list">
-                <li><a href="/index.php">Inicio</a></li>
-                <li><a href="/usuario/cadastro.php">Cadastrar</a></li>
-                <li><a href="/usuario/login.php">Entrar</a></li>
-                <li><a href="/catalogo/catalogo.php">Catalogo</a></li>
-            </ul>
-            <?php else: ?>
-            <ul class="nav-list">
-                <li><a href="/index.php">Inicio</a></li>
-                <li><a href="/catalogo/catalogo.php">Catalogo</a></li>
-                <li><a href="/usuario/logout.php">Sair</a></li>
-                <li><a href=""><?= ucfirst($_SESSION['userL']) ?></a></li>
-            </ul>
-            <?php endif ?>
+            <?php include '../usuario/navbar.php'; ?>
         </nav>
     </header>
     <div class="obra">
