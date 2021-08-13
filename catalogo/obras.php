fb0c10dd62c0670163f6630e9b01d22979ff0892
gustavojms <gustavo.joset0@hotmail.com>
2021-08-13 18:03:14 -0300

    Alteraçao na navbar e ajustes no botão de excluir

--- catalogo/obras.php
+++ catalogo/obras.php
@@ -7,48 +7,34 @@
     $data = $result -> fetchAll();
     $stmt = $pdo -> query("SELECT * FROM comentario WHERE id_obra_cmt = '$id'");
     $dado = $stmt -> fetchAll();
-?>
+    ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Obras - TTMCinema</title>
-    <link rel="stylesheet" href="/assets/css/style.css">
-    <link rel="icon" href="/assets/img/moon.png" type="image/x-icon">
-</head>
-<body>
-    <header>
-        <nav>
-            <a class="logo">TTMCinema</a>
-            <div class="mobile-menu">
-                <div class="line1"></div>
-                <div class="line2"></div>
-                <div class="line3"></div>
-            </div>
-            <?php if (!Usuario::isLogged()): ?>
-            <ul class="nav-list">
-                <li><a href="/index.php">Inicio</a></li>
-                <li><a href="/usuario/cadastro.php">Cadastrar</a></li>
-                <li><a href="/usuario/login.php">Entrar</a></li>
-                <li><a href="/catalogo/catalogo.php">Catalogo</a></li>
-            </ul>
-            <?php else: ?>
-            <ul class="nav-list">
-                <li><a href="/index.php">Inicio</a></li>
-                <li><a href="/catalogo/catalogo.php">Catalogo</a></li>
-                <li><a href="/usuario/logout.php">Sair</a></li>
-                <li><a href=""><?= ucfirst($_SESSION['userL']) ?></a></li>
-            </ul>
-            <?php endif ?>
-        </nav>
-    </header>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Obras - TTMCinema</title>
+        <link rel="stylesheet" href="/assets/css/style.css">
+        <link rel="icon" href="/assets/img/moon.png" type="image/x-icon">
+    </head>
+    <body>
+        <header>
+            <nav>
+                <a class="logo">TTMCinema</a>
+                <div class="mobile-menu">
+                    <div class="line1"></div>
+                    <div class="line2"></div>
+                    <div class="line3"></div>
+                </div>
+               <?php include '../usuario/navbar.php'; ?>
+            </nav>
+        </header>
 <main>
     <table>
         <tr>
-            <th>Titulo</th>
+            <th>Título</th>
             <th>Ano de lançamento</th>
             <th>Classificação Etária</th>
             <th>Tipo</th>
@@ -70,32 +56,33 @@
         <?php endforeach ?>
     </table>
 
-    <div class="comment-box">
-
-    <form action="/comentarios/add-comment.php?id_obra=<?= $row['id_obra'] ?>" method="POST">
-        <label for="comentario">
-            <textarea name="comentario" id="comentario" style="resize: none" placeholder="Escrever um comentário..."></textarea>
-        </label>
-
-    <button type="submit">Comentar</button>
-    </form>
-    </div>
-
     <table class="comentario">
         <tr>
-            <th>Comentarios</th>
+            <th>Comentários</th>
+            <th>Data do comentário</th>
         </tr>
         <tr>
             <?php foreach($dado as $dados): ?>
                 <td><?= $dados['comentario']?></td>
+                <td><?= $dados['dataAtual']?></td>
                 <td>
             <?php if (Usuario::isLogged() && $dados['id_user_cmt'] == $_SESSION['userId']): ?>
-                    <a href="/comentarios/remove-comentario.php?id_obra=<?= $dados['id_obra_cmt'] ?>"> Excluir </a>        
+                    <a href="/comentarios/remove-comentario.php?id_obra=<?= $dados['id_obra_cmt'] ?>&id_comentario=<?= $dados['id_comentario'] ?>"> Excluir </a>        
             <?php endif ?>
                 </td>
         </tr>
         <?php endforeach ?>
     </table>
+
+    <div class="comment-box">
+    <form action="/comentarios/add-comment.php?id_obra=<?= $row['id_obra'] ?>" method="POST">
+        <label for="comentario">
+            <textarea name="comentario" id="comentario" style="resize: none" placeholder="Escrever um comentário..."></textarea>
+        </label>
+    <button type="submit">Comentar</button>
+    </form>
+    </div>
+
 </main>
 <section class="images">
     <div class="circle"></div>
