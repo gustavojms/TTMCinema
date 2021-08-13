679627a5a1c322622fe458e302947e37d45ee945
gustavojms <gustavo.joset0@hotmail.com>
2021-08-13 18:04:20 -0300

    Adicionado método de segurança e ajuste no sql

--- comentarios/add-comment.php
+++ comentarios/add-comment.php
@@ -1,5 +1,6 @@
 <?php
     include '../conexao/conexao.php';
+    include '../usuario/UsuarioClass.php';
     $id = $_GET['id_obra'];
 
     if (isset($_POST['comentario']) && !empty($_POST['comentario'])) {
@@ -9,10 +10,11 @@
         header("Location: /catalogo/obras.php?id_obra=$id");
     }
 
-    $sql = $pdo -> query("SELECT id_obra FROM obra");
-    $data = $sql -> fetchAll();
+    if(!Usuario::isLogged()) {
+        header("Location: /usuario/login.php");
+    }
 
-    $result = $pdo -> prepare("INSERT INTO comentario (comentario, id_obra_cmt, id_user_cmt) VALUES(?, ?, ?)");
+    $result = $pdo -> prepare("INSERT INTO comentario (comentario, dataAtual, id_obra_cmt, id_user_cmt) VALUES(?, NOW(), ?, ?)");
     $result -> execute([$comentario, $id, $_SESSION['userId']]);
 
     header("Location: /catalogo/obras.php?id_obra=$id");
