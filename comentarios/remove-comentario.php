a2e06bf1a42aa009cf273c6845db315f007b6180
gustavojms <gustavo.joset0@hotmail.com>
2021-08-13 18:04:38 -0300

    Ajustes no sql

--- comentarios/remove-comentario.php
+++ comentarios/remove-comentario.php
@@ -3,9 +3,10 @@
     include '../usuario/UsuarioClass.php';
 
     $id = $_GET['id_obra'];
+    $idcmt = $_GET['id_comentario'];
 
-    $result = $pdo -> prepare("DELETE FROM comentario WHERE id_user_cmt = ? AND id_obra_cmt = ?");
-    $result -> execute([$_SESSION['userId'], $id]);
+    $result = $pdo -> prepare("DELETE FROM comentario WHERE id_user_cmt = ? AND id_comentario = ?");
+    $result -> execute([$_SESSION['userId'], $idcmt]);
 
     header("Location: /catalogo/obras.php?id_obra=$id");
 ?>
